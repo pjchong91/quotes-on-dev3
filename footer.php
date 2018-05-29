db155d1f019467b0868d12b3b249c56dd2be704c
Pamela Chong <pjchong91@gmail.com>
2018-05-29 15:27:35 -0700

    Styled index.php for mobile

--- footer.php
+++ footer.php
@@ -20,7 +20,7 @@
 						 	) 
 						); 
 					?>
-					<p>Brought to you by <a href="http://www.redacademy.com/">RED Academy</a></p>	
+					<p class="footer-text">Brought to you by <a href="http://www.redacademy.com/">RED Academy</a></p>	
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
