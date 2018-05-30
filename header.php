ec00569b1d1d1ce43c77222d146b6a77a217d971
Pamela Chong <pjchong91@gmail.com>
2018-05-29 23:43:47 -0700

    Styled About, Archive, Search, and 404 pages

--- header.php
+++ header.php
@@ -12,6 +12,7 @@
 		 <meta name="viewport" content="width=device-width, initial-scale=1">
 		 <link rel="profile" href="http://gmpg.org/xfn/11">
 		 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+		 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
 
 	 <?php wp_head(); ?>
 	 </head>
