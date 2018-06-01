4d3b858fb86be5fef688ed827b73b588811e1ed2
Pamela Chong <pjchong91@gmail.com>
2018-06-01 15:52:38 -0700

    Removed link in header to font-awesome

--- header.php
+++ header.php
@@ -12,7 +12,6 @@
 		 <meta name="viewport" content="width=device-width, initial-scale=1">
 		 <link rel="profile" href="http://gmpg.org/xfn/11">
 		 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-		 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
 		 <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
 	 <?php wp_head(); ?>
 	 </head>
